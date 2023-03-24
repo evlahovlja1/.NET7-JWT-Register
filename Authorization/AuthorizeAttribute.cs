3d0434a7430a94f38ca59b791975727e5fedbca4
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2023-03-24 10:00:19 +0100

    attibute custom name

--- Authorization/AuthorizeAttribute.cs
+++ Authorization/AuthorizeAttribute.cs
@@ -4,13 +4,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using WebApi.Entities;
 
+//Custom added to override Identity's attribute with same name
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+public class CustomAuthorizeAttribute : Attribute, IAuthorizationFilter
 {
     public void OnAuthorization(AuthorizationFilterContext context)
     {
         // skip authorization if action is decorated with [AllowAnonymous] attribute
-        var allowAnonymous = context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any();
+        var allowAnonymous = context.ActionDescriptor.EndpointMetadata.OfType<CustomAllowAnonymousAttribute>().Any();
         if (allowAnonymous)
             return;
 
